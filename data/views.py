2b01bc54d9d605edc30b93da17d6319d9405e31e
Gavin Osborn <osborng@xavier.edu>
2022-11-10 13:44:21 -0500

    added methods to the Major viewset, but unverified. Hoping someone can look at this and see what direction this is going in.

--- data/views.py
+++ data/views.py
@@ -1,10 +1,16 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from data.serializers import DepartmentSerializer, MajorSerializer, MinorSerializer
 from data.serializers import StudentSerializer, ProfessorSerializer, AdminAssistantSerializer
 from data.serializers import CourseSerializer, HighImpactExperienceSerializer, EventSerializer
-from data.models import Department, Major, Minor, Student, Professor, AdminAssistant, Course, HighImpactExperience, Event
+from data.models import Department, Major, Minor, Student, Professor, AdminAssistant, Course, HighImpactExperience, \
+    Event
 from rest_framework import viewsets, permissions
+
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.request import Request
+
 
 def api(request):
     return HttpResponse("API Home Page")
@@ -15,12 +21,58 @@ class DepartmentViewSet(viewsets.ModelViewSet):
     serializer_class = DepartmentSerializer
     permission_classes = [permissions.AllowAny]
 
+    def list(self, request):
+        queryset = Department.objects.all()
+        serializer_class = DepartmentSerializer
+        permission_classes = [permissions.AllowAny]
+        return HttpResponse(serializer_class.data)
+
+    def retrieve(self, request, pk=None):
+        queryset = Department.objects.all()
+        user = get_object_or_404(queryset, pk=pk)
+        serializer_class = DepartmentSerializer
+        permission_classes = [permissions.AllowAny]
+        return HttpResponse(serializer_class.data)
+
 
 class MajorViewSet(viewsets.ModelViewSet):
     queryset = Major.objects.all()
     serializer_class = MajorSerializer
     permission_classes = [permissions.AllowAny]
 
+    @action(methods=["DELETE"], detail=False, permission_classes=[permissions.AllowAny])
+    def delete(self, request: Request):
+        queryset = Major.objects.all()
+        delete_id = request.data
+        delete_albums = self.queryset.filter(id__in=delete_id)
+        delete_albums.delete()
+        return Response(self.serializer_class(delete_albums, many=True).data)
+
+    def list(self, request):
+        queryset = Major.objects.all()
+        serializer_class = MajorSerializer
+        permission_classes = [permissions.AllowAny]
+        return HttpResponse(serializer_class.data)
+
+    def retrieve(self, request, pk=None):
+        queryset = Major.objects.all()
+        user = get_object_or_404(queryset, pk=pk)
+        serializer_class = MajorSerializer
+        permission_classes = [permissions.AllowAny]
+        return HttpResponse(serializer_class.data)
+
+    def update(self, request, *args, **kwargs):
+
+        queryset = Major.objects.all()
+        serializer_class = MajorSerializer
+        permission_classes = [permissions.AllowAny]
+
+        instance = self.get_object()
+        instance.major = request.data.get("Major")
+        instance.save()
+        self.perform_update(serializer_class)
+
+        return HttpResponse(serializer_class.data)
 
 class MinorViewSet(viewsets.ModelViewSet):
     queryset = Minor.objects.all()
@@ -61,4 +113,4 @@ class HighImpactExperienceViewSet(viewsets.ModelViewSet):
 class EventViewSet(viewsets.ModelViewSet):
     queryset = Event.objects.all()
     serializer_class = EventSerializer
-    permission_classes = [permissions.DjangoModelPermissions] 
+    permission_classes = [permissions.DjangoModelPermissions]
