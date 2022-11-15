4289cf690d3f30ca52daf8e964ffefe6eb98f076
JaxonArnold <arnoldj20@stxtigers.com>
2022-11-15 13:10:45 -0500

    Update to Viewsets

--- data/views.py
+++ data/views.py
@@ -12,9 +12,37 @@ def api(request):
     return HttpResponse("API Home Page")
 
 
-class DepartmentViewSet(viewsets.ModelViewSet):
-    queryset = Department.objects.all()
-    serializer_class = DepartmentSerializer
+class DepartmentViewSet(viewsets.ViewSet):
+    
+    def create(self, request):
+        queryset = Department.objects.all()
+        serializer = DepartmentSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    
+    def update(self, request, pk=None):
+        queryset = Department.objects.all()
+        department = get_object_or_404(queryset, pk=pk)
+        serializer = DepartmentSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def delete(self, request, pk=None):
+         queryset = Department.objects.all()
+         department = get_object_or_404(queryset, pk=pk)
+         department.delete()
+
+    
+    def list(self, request):
+        queryset = Department.objects.all()
+        serializer = DepartmentSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        queryset = Department.objects.all()
+        department = get_object_or_404(queryset, pk=pk)
+        serializer = DepartmentSerializer(department)
+        return Response(serializer.data)
+
     permission_classes = [permissions.AllowAny]
 
 
@@ -52,7 +80,7 @@ class MajorViewSet(viewsets.ViewSet):
     permission_classes = [permissions.AllowAny]
 
 
-class MinorViewSet(viewsets.ModelViewSet):
+class MinorViewSet(viewsets.ViewSet):
     
     def create(self, request):
         queryset = Minor.objects.all()
@@ -86,37 +114,205 @@ class MinorViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.AllowAny]
 
 
-class StudentViewSet(viewsets.ModelViewSet):
-    queryset = Student.objects.all()
-    serializer_class = StudentSerializer
+class StudentViewSet(viewsets.ViewSet):
+    
+    def create(self, request):
+        queryset = Student.objects.all()
+        serializer = StudentSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    
+    def update(self, request, pk=None):
+        queryset = Student.objects.all()
+        student = get_object_or_404(queryset, pk=pk)
+        serializer = StudentSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def delete(self, request, pk=None):
+         queryset = Student.objects.all()
+         student = get_object_or_404(queryset, pk=pk)
+         student.delete()
+
+    
+    def list(self, request):
+        queryset = Student.objects.all()
+        serializer = StudentSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        queryset = Student.objects.all()
+        student = get_object_or_404(queryset, pk=pk)
+        serializer = StudentSerializer(student)
+        return Response(serializer.data)
+
     permission_classes = [permissions.AllowAny]
 
 
-class ProfessorViewSet(viewsets.ModelViewSet):
-    queryset = Professor.objects.all()
-    serializer_class = ProfessorSerializer
-    permission_classes = [permissions.DjangoModelPermissions]
+class ProfessorViewSet(viewsets.ViewSet):
+    
+    def create(self, request):
+        queryset = Professor.objects.all()
+        serializer = ProfessorSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    
+    def update(self, request, pk=None):
+        queryset = Professor.objects.all()
+        professor = get_object_or_404(queryset, pk=pk)
+        serializer = ProfessorSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def delete(self, request, pk=None):
+         queryset = Professor.objects.all()
+         professor = get_object_or_404(queryset, pk=pk)
+         professor.delete()
+
+    
+    def list(self, request):
+        queryset = Professor.objects.all()
+        serializer = ProfessorSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        queryset = Professor.objects.all()
+        professor = get_object_or_404(queryset, pk=pk)
+        serializer = ProfessorSerializer(professor)
+        return Response(serializer.data)
+
+    permission_classes = [permissions.AllowAny]
 
 
-class AdminAssistantViewSet(viewsets.ModelViewSet):
-    queryset = AdminAssistant.objects.all()
-    serializer_class = AdminAssistantSerializer
-    permission_classes = [permissions.DjangoModelPermissions]
+class AdminAssistantViewSet(viewsets.ViewSet):
+    
+    def create(self, request):
+        queryset = AdminAssistant.objects.all()
+        serializer = AdminAssistantSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    
+    def update(self, request, pk=None):
+        queryset = AdminAssistant.objects.all()
+        adminassistant = get_object_or_404(queryset, pk=pk)
+        serializer = AdminAssistantSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def delete(self, request, pk=None):
+         queryset = AdminAssistant.objects.all()
+         adminassistant = get_object_or_404(queryset, pk=pk)
+         adminassistant.delete()
+
+    
+    def list(self, request):
+        queryset = AdminAssistant.objects.all()
+        serializer = AdminAssistantSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        queryset = AdminAssistant.objects.all()
+        adminassistant = get_object_or_404(queryset, pk=pk)
+        serializer = AdminAssistantSerializer(adminassistant)
+        return Response(serializer.data)
+
+    permission_classes = [permissions.AllowAny]
 
 
-class CourseViewSet(viewsets.ModelViewSet):
-    queryset = Course.objects.all()
-    serializer_class = CourseSerializer
-    permission_classes = [permissions.DjangoModelPermissions]
+class CourseViewSet(viewsets.ViewSet):
+    
+    def create(self, request):
+        queryset = Course.objects.all()
+        serializer = CourseSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    
+    def update(self, request, pk=None):
+        queryset = Course.objects.all()
+        course = get_object_or_404(queryset, pk=pk)
+        serializer = CourseSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def delete(self, request, pk=None):
+         queryset = Course.objects.all()
+         course = get_object_or_404(queryset, pk=pk)
+         course.delete()
+
+    
+    def list(self, request):
+        queryset = Course.objects.all()
+        serializer = CourseSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        queryset = Course.objects.all()
+        course = get_object_or_404(queryset, pk=pk)
+        serializer = CourseSerializer(course)
+        return Response(serializer.data)
+
+    permission_classes = [permissions.AllowAny]
 
 
-class HighImpactExperienceViewSet(viewsets.ModelViewSet):
-    queryset = HighImpactExperience.objects.all()
-    serializer_class = HighImpactExperienceSerializer
-    permission_classes = [permissions.DjangoModelPermissions]
+class HighImpactExperienceViewSet(viewsets.ViewSet):
+    
+    def create(self, request):
+        queryset = HighImpactExperience.objects.all()
+        serializer = HighImpactExperienceSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    
+    def update(self, request, pk=None):
+        queryset = HighImpactExperience.objects.all()
+        highImpactExperience = get_object_or_404(queryset, pk=pk)
+        serializer = HighImpactExperienceSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def delete(self, request, pk=None):
+         queryset = HighImpactExperience.objects.all()
+         highImpactExperience = get_object_or_404(queryset, pk=pk)
+         highImpactExperience.delete()
+
+    
+    def list(self, request):
+        queryset = HighImpactExperience.objects.all()
+        serializer = HighImpactExperienceSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        queryset = HighImpactExperience.objects.all()
+        highImpactExperience = get_object_or_404(queryset, pk=pk)
+        serializer = HighImpactExperienceSerializer(highImpactExperience)
+        return Response(serializer.data)
+
+    permission_classes = [permissions.AllowAny]
 
 
-class EventViewSet(viewsets.ModelViewSet):
-    queryset = Event.objects.all()
-    serializer_class = EventSerializer
-    permission_classes = [permissions.DjangoModelPermissions] 
+class EventViewSet(viewsets.ViewSet):
+    
+    def create(self, request):
+        queryset = Event.objects.all()
+        serializer = EventSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    
+    def update(self, request, pk=None):
+        queryset = Event.objects.all()
+        event = get_object_or_404(queryset, pk=pk)
+        serializer = EventSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def delete(self, request, pk=None):
+         queryset = Event.objects.all()
+         event = get_object_or_404(queryset, pk=pk)
+         event.delete()
+
+    
+    def list(self, request):
+        queryset = Event.objects.all()
+        serializer = EventSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        queryset = Event.objects.all()
+        event = get_object_or_404(queryset, pk=pk)
+        serializer = EventSerializer(event)
+        return Response(serializer.data)
+
+    permission_classes = [permissions.AllowAny] 
